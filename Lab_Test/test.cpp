94de82779e638987c0a24c134ffa226f0a3efc5e
Silvasan <74051655+Silvasan@users.noreply.github.com>
2020-12-11 09:55:29 +0300

    vers1

--- Lab_Test/test.cpp
+++ Lab_Test/test.cpp
@@ -1,8 +1,15 @@
 #include "pch.h"
 #include "C:/Users/Silver/Desktop/Учебники/ГУАП/Информатика/1 семестр. Си/Laba03/Laba03/Library.cpp"
-namespace LiraryUnitTests {
-TEST(delete_spacesTest, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
-}
+
+	TEST(Testdelete_spaces, Only_spaces) {
+		char str1[125] = "   \t    \t   \n", *str2 = "\n";
+		ASSERT_EQ(delete_spaces(str1), *str2);
+	}
+	TEST(Testdelete_spaces, Empty_string) {
+		char str1[125] = "\n", *str2 = "\n";
+		ASSERT_EQ(delete_spaces(str1), *str2);
+	}
+	TEST(Testdelete_spaces, Correct) {
+		char str1[125] = " \t stri\tng 0123 \t \n", *str2 = "string 0123\n";
+		ASSERT_EQ(delete_spaces(str1), *str2);
+	}
